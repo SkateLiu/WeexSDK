25290bb2c7bb0de00420703ce180f42be8246f23
liuqiang <774954328@qq.com>
2019-05-29 00:02:37 +0800

    add file

---
+++ WeexSDK/WeexSDK/WeexSDK/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  WeexSDK
+//
+//  Created by liuqiang on 2019/5/28.
+//  Copyright © 2019 liuqiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WeexSDK/WeexSDK/WeexSDK/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  WeexSDK
+//
+//  Created by liuqiang on 2019/5/28.
+//  Copyright © 2019 liuqiang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
